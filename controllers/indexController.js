5accf926515aa3a2aec4a4f6de49732ae15846e6
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-11-13 18:37:55 +0200

    Add the app, the router and the controller for home page

---
+++ controllers/indexController.js
@@ -0,0 +1,5 @@
+function indexController(req, res) {
+  res.send('Hello World');
+}
+
+module.exports = {indexController};
---
+++ routes/indexRouter.js
@@ -0,0 +1,6 @@
+const {Router} = require('express');
+const {indexController} = require('../controllers/indexController');
+const indexRouter = Router();
+indexRouter.get('/', indexController);
+
+module.exports = {indexRouter};
