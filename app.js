1a8dd9c4c5511d3b148f6cccdfebcdb9d1f6f503
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-11-13 19:26:22 +0200

    Define EJS as the view engine

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;  
 const {indexRouter} = require('./routes/indexRouter');
 const {newRouter} = require('./routes/newRouter');
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use('/', indexRouter);
 app.use('/new', newRouter);
 app.listen(PORT, () => {
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,6 +1,21 @@
 const {Router} = require('express');
 const {indexController} = require('../controllers/indexController');
 const indexRouter = Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+
 indexRouter.get('/', indexController);
 
 module.exports = {indexRouter};
