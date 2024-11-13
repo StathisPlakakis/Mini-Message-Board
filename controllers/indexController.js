fff3cfbaf9465a1d8c5c4676aa69046a66b76ec7
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-11-13 20:08:37 +0200

    Prepeare the template for rendering the messages

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,18 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 function indexController(req, res) {
-  res.send('Hello World');
+  res.render('index', {messages: messages});
 }
 
 module.exports = {indexController};
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,20 +2,6 @@ const {Router} = require('express');
 const {indexController} = require('../controllers/indexController');
 const indexRouter = Router();
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
-];
-
-
 indexRouter.get('/', indexController);
 
 module.exports = {indexRouter};
